81fb258659facc3300cf21ccc4dbd9e9e0e327fe
AndrosDe <andreas_beier1981@yahoo.de>
2022-09-28 15:24:25 +0000

    Added category filtering

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
+'''Imports'''
 from django.db import models
 
 
-
 class Category(models.Model):
-
+    '''Category Model'''
     class Meta:
+        '''Category Model Meta'''
         verbose_name_plural = "Categories"
 
     name = models.CharField(max_length=254)
@@ -14,16 +15,26 @@ class Category(models.Model):
         return self.name
 
     def get_friendly_name(self):
+        '''Category Model Friendly Name'''
         return self.friendly_name
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    '''Product Model'''
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
